7384642c8596550a3ccda927d3881dc8ebcde558
mgueyraud <mgueyraud.junior@gmail.com>
2019-07-08 23:08:38 -0400

    Fin de la seccion 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,6 @@ import { InfoPaginaService } from './services/info-pagina.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private infoPaginaService: InfoPaginaService){}
+  constructor(public infoPaginaService: InfoPaginaService,
+              public productosService: ProductosService){}
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,12 +9,25 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.info = resp;
         this.cargada = true;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-60b82.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
         console.log(resp);
       });
   }
